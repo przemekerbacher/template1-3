7c0c20555b8e76141cd6fd89a7c6a8d5017b52be
Przemek <przemyslaw.erbaher@gmail.com>
2020-04-16 15:54:17 +0200

    init

---
+++ js/site.js
@@ -0,0 +1,139 @@
+const extraButton = document.querySelector(".buttons .expand");
+const addToBasketButtons = document.querySelectorAll(".add-to-basket");
+
+if (extraButton) {
+  extraButton.addEventListener("click", function () {
+    const extraMenu = document.querySelector(".buttons");
+    extraMenu.classList.toggle("active");
+  });
+}
+
+if (addToBasketButtons) {
+  addToBasketButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      const iconAdd = button.querySelector(".add");
+      const iconOk = button.querySelector(".ok");
+
+      button.classList.toggle("active");
+      if (iconAdd) iconAdd.classList.toggle("hide");
+      if (iconOk) iconOk.classList.toggle("hide");
+    });
+  });
+}
+
+//add collapsible effect
+const collasibleElements = document.querySelectorAll(
+  ".collapsible, .button-collapsible"
+);
+
+if (collasibleElements)
+  collasibleElements.forEach((element) => {
+    element.addEventListener("click", function (e) {
+      const content = document.querySelector(
+        e.currentTarget.getAttribute("data-target")
+      );
+
+      e.currentTarget.classList.toggle("active");
+
+      if (content)
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+          content.classList.remove("active");
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+          content.classList.add("active");
+        }
+    });
+  });
+
+//add active class when scroll
+window.addEventListener("scroll", function () {
+  const mainNavigation = document.querySelector(".main-navigation");
+  const collapsibleButton = document.querySelector(".button-collapsible");
+
+  if (window.scrollY > 0) {
+    if (mainNavigation) {
+      mainNavigation.classList.add("scroll-active");
+      mainNavigation.classList.add("tigh");
+    }
+  } else {
+    {
+      mainNavigation.classList.remove("scroll-active");
+      mainNavigation.classList.remove("tigh");
+    }
+  }
+});
+
+//add active clas when button clicked
+const collapsibleButton = document.querySelector(".button-collapsible");
+
+collapsibleButton.addEventListener("click", function () {
+  const mainNavigation = document.querySelector(".main-navigation");
+  this.classList.toggle("is-active");
+  if (mainNavigation) {
+    if (this.classList.contains("active")) {
+      mainNavigation.classList.add("active");
+    } else {
+      mainNavigation.classList.remove("active");
+    }
+  }
+});
+
+//
+const reservation = document.querySelector("#reserve");
+
+if (reservation) {
+  const moreInformation = document.querySelector(".hidden-form");
+  reservation.addEventListener("click", function (e) {
+    e.preventDefault();
+    moreInformation;
+  });
+}
+
+//modal
+const modalButtons = document.querySelectorAll(".show-modal");
+if (modalButtons) {
+  modalButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const modal = document.querySelector(
+        e.currentTarget.getAttribute("data-target")
+      );
+
+      if (modal) {
+        const closeModalButton = modal.querySelector(".close");
+
+        const closeThisModal = () => {
+          modal.style.display = "none";
+          document.body.style.overflow = "auto";
+        };
+
+        const showThisModal = () => {
+          modal.style.display = "block";
+          document.body.style.overflow = "hidden";
+        };
+
+        //handle show modal
+        showThisModal();
+
+        //Handle close modal
+        window.addEventListener("click", function (e) {
+          if (e.target == modal) {
+            closeThisModal();
+          }
+        });
+        if (closeModalButton) {
+          closeModalButton.addEventListener("click", closeThisModal);
+        }
+      }
+    });
+  });
+}
+
+//enable parallax
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".parallax");
+  M.Parallax.init(elems);
+});
